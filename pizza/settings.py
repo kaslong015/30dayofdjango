6a8945eb1596295b8cc1087387479f5074375a2e
kaslong <kevinkaslong@gmail.com>
2022-08-04 22:48:50 +0100

    updated database in settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -108,17 +108,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pizza.wsgi.application'
-
+DATABASES = {}
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 if DEBUG:
-    DATABASES = {
-        'default': {
+    DATABASES['default']={
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+            'NAME': BASE_DIR / 'db.sqlite3'}
+    
 else:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
     DATABASES['default'] = dj_database_url.config(
